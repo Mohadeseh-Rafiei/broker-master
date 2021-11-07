90d8065bac95c5787a29b4e51edec594dec81b90
Mohadeseh-Rafiei <mohadeserphy@gmail.com>
2021-11-07 14:52:41 +0330

    addd redis

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module main
 go 1.15
 
 require (
-	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.16.0 // indirect
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- internal/broker/module.go
+++ internal/broker/module.go
@@ -2,102 +2,129 @@ package broker
 
 import (
 	"context"
+	"github.com/go-redis/redis"
 	"main/pkg/broker"
-	"sync"
-	"time"
 )
 
+//TODO : BOTTLENECK IN CHANS
+//TODO : ALL LOCKS
+
+//TODO : SEND MASSAGE FOR EVERY CLIENT OR REDIS MEM CAN HELP
+
 type Module struct {
-	chat map[string] []broker.Message
-	clients map[string][]*chan broker.Message
-	expiration map[string][]bool
-	close bool
-	mx sync.Mutex
+	*redis.Client
+	subs map[string]*redis.PubSub
 }
 
+//
+//type Module struct {
+//	chat map[string] []broker.Message
+//	clients map[string][]*chan broker.Message
+//	expiration map[string][]bool
+//	close bool
+//	mx sync.Mutex
+//}
+
 func NewModule() broker.Broker {
+	client := redis.NewClient(&redis.Options{Addr: ":3679"})
 	return &Module{
-			chat: map[string][]broker.Message{},
-			clients: map[string][]*chan broker.Message{},
-			expiration: map[string][]bool{},
-			close: false,
+		Client: client,
+		subs:   make(map[string]*redis.PubSub),
 	}
 }
 
-
 func (m *Module) Close() error {
-	m.close = true
-	m.mx.Lock()
-	defer m.mx.Unlock()
-	for _, elements := range m.clients{
-		for _, element := range elements{
-			close(*element)
-		}
+	if err := m.Client.Close(); err != nil {
+		return err
 	}
 	return nil
 }
 
 func (m *Module) Publish(ctx context.Context, subject string, msg broker.Message) (int, error) {
-	if m.close{
+	//if m.close{
+	//	return 0, broker.ErrUnavailable
+	//}
+	//m.mx.Lock()
+	//m.chat[subject] = append(m.chat[subject], msg)
+	//m.expiration[subject] = append(m.expiration[subject], false)
+	//wg := &sync.WaitGroup{}
+	//for _, n := range m.clients[subject]{
+	//	wg.Add(1)
+	//	go func(wg *sync.WaitGroup, ) {
+	//		*n <- msg
+	//		wg.Done()
+	//	}(wg)
+	//
+	//}
+	//wg.Wait()
+	//index := len(m.chat[subject]) - 1
+	//m.mx.Unlock()
+	//go func() {
+	//	if msg.Expiration == 0 {
+	//		return
+	//	}
+	//	ticker := time.NewTicker(msg.Expiration)
+	//	<-ticker.C
+	//	m.expiration[subject][index] = true
+	//}()
+	//return index, nil
+
+	_, ok := m.subs[subject]
+	if !ok {
 		return 0, broker.ErrUnavailable
 	}
-	m.mx.Lock()
-	m.chat[subject] = append(m.chat[subject], msg)
-	m.expiration[subject] = append(m.expiration[subject], false)
-	wg := &sync.WaitGroup{}
-	for _, n := range m.clients[subject]{
-		wg.Add(1)
-		go func(wg *sync.WaitGroup, ) {
-			*n <- msg
-			wg.Done()
-		}(wg)
-
+	if err := m.Client.Publish(subject, msg).Err(); err != nil {
+		return 0, err
 	}
-	wg.Wait()
-	index := len(m.chat[subject]) - 1
-	m.mx.Unlock()
-	go func() {
-		if msg.Expiration == 0 {
-			return
-		}
-		ticker := time.NewTicker(msg.Expiration)
-		<-ticker.C
-		m.expiration[subject][index] = true
-	}()
-	return index, nil
+	return len(m.subs) - 1, nil
 }
 
 func (m *Module) Subscribe(ctx context.Context, subject string) (<-chan broker.Message, error) {
-	if m.close{
-		empty_chan := make(chan broker.Message)
-		return empty_chan, broker.ErrUnavailable
-	}
-	out_chan := make(chan broker.Message, 1000000)
-	m.mx.Lock()
-	//wg := &sync.WaitGroup{}
-	//
-	//for _, n := range m.chat[subject]{
-	//	wg.Add(1)
-	//	go func(wg *sync.WaitGroup) {
-	//		out_chan <- n
-	//		wg.Done()
-	//	}(wg)
+	//if m.close{
+	//	empty_chan := make(chan broker.Message)
+	//	return empty_chan, broker.ErrUnavailable
 	//}
-	//wg.Wait()
-	m.clients[subject] = append(m.clients[subject], &out_chan)
-	m.mx.Unlock()
-	return  out_chan, nil
+	//out_chan := make(chan broker.Message, 1000000)
+	//m.mx.Lock()
+	////wg := &sync.WaitGroup{}
+	////
+	////for _, n := range m.chat[subject]{
+	////	wg.Add(1)
+	////	go func(wg *sync.WaitGroup) {
+	////		out_chan <- n
+	////		wg.Done()
+	////	}(wg)
+	////}
+	////wg.Wait()
+	//m.clients[subject] = append(m.clients[subject], &out_chan)
+	//m.mx.Unlock()
+	//return  out_chan, nil
+
+	sub := m.Client.Subscribe(subject)
+	_, err := sub.Receive()
+	if err != nil {
+		return nil, broker.ErrUnavailable
+	}
+	m.subs[subject] = sub
+	out := make(chan broker.Message)
+	go func() {
+		for massage := range sub.Channel() {
+			out <- massage
+		}
+		close(out)
+	}()
+	return out, nil
 }
 
 func (m *Module) Fetch(ctx context.Context, subject string, id int) (broker.Message, error) {
-	m.mx.Lock()
-	defer m.mx.Unlock()
-	msg := broker.Message{}
-	if m.close{
-		return msg, broker.ErrUnavailable
-	}
-	if m.expiration[subject][id] {
-		return msg, broker.ErrExpiredID
-	}
-	return m.chat[subject][id], nil
+	//m.mx.Lock()
+	//defer m.mx.Unlock()
+	//msg := broker.Message{}
+	//if m.close{
+	//	return msg, broker.ErrUnavailable
+	//}
+	//if m.expiration[subject][id] {
+	//	return msg, broker.ErrExpiredID
+	//}
+	//return m.chat[subject][id], nil
 }
